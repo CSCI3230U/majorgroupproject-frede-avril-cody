c6f31d90955db79bdfc4250f3603cf5926f6d761
FredeEmnetu <ferid226@hotmail.com>
2021-04-12 00:38:07 -0400

    made links to different components

--- twitter_clone/src/components/Menu.js
+++ twitter_clone/src/components/Menu.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Menu extends Component{
     render(){
         return(
             <>
                 <ul>
-                    <li>Home</li>
-                    <li>Explore</li>
-                    <li>Messages</li>
+                    <li><Link to="/feed">Home</Link></li>
+                    <li><Link to="/">Profile</Link></li>
+                    <li><Link to="/messages">Messages</Link></li>
                     <li>Connect</li>
-                    <li>Profile</li>
-                    <li>More</li>
-                    
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li>More</li> 
                 </ul>
                 <div className=".btn">
                     <button type="button">Tweet</button>
