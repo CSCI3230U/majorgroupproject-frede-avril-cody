9e6aa3e1d576ab04ce536c387556277a5d1c48d2
FredeEmnetu <ferid226@hotmail.com>
2021-04-13 20:38:53 -0400

    Error Page

---
+++ twitter_clone/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+    render(){
+        return (
+            <>
+                <h1>This page does not exist</h1>
+               
+            </>
+        );
+    }
+}
+
+export default App;
