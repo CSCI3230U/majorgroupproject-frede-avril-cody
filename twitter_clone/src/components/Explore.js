374278b97736458b78265c9dc54d66b3dfbc79dd
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-04-15 20:56:17 -0400

    Figure out database setup

--- twitter_clone/src/components/Explore.js
+++ twitter_clone/src/components/Explore.js
@@ -3,6 +3,17 @@ import { withRouter } from 'react-router-dom';
 import '../styles/Explore.css';
 
 class Explore extends Component {
+
+    /* DEMO OF HOW TO RETRIEVE DATABASE DATA */
+    componentDidMount() {
+        fetch("http://localhost:4000/tweets")
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+            });
+    }
+    /* END OF DEMO */
+
     render() {
         return(
             <div className={`explore`}>
