164d86a3651b60426f101bd54b13f404d0814256
Avril <avril.van.domselaar@hotmail.com>
2021-04-17 22:57:42 -0300

    More menu styling

--- twitter_clone/src/components/Menu.js
+++ twitter_clone/src/components/Menu.js
@@ -27,29 +27,55 @@ class Menu extends Component{
         return(
             <div className={`menu-menu`}>
                 <FontAwesomeIcon className={`menu-twitter-icon`} icon={faTwitter} size="2x" />
-                <div className={`menu-row inline`}>
-                    <FontAwesomeIcon className={`inline menu-home-icon`} icon={faHome} size="2x" />
-                    <Link to="/feed" className={`inline menu-option menu-home`}>Home</Link>
-                </div>
-                <div className={`menu-row inline`}>
-                    <FontAwesomeIcon className={`inline`} icon={faHashtag} size="2x" />
-                    <Link to="/explore" className={`menu-option`}>Explore</Link>
-                </div>
-                <div className={`menu-row inline`}>
-                    <FontAwesomeIcon className={`inline`} icon={faEnvelope} size="2x" />
-                    <Link to="/messages" className={`menu-option`}>Messages</Link>
-                </div>
-                <div className={`menu-row inline`}>
-                    <FontAwesomeIcon className={`inline`} icon={faUsers} size="2x" />
-                    <Link to="/connect" className={`menu-option`}>Connect</Link>
-                </div>
-                <div className={`menu-row inline`}>
-                    <FontAwesomeIcon className={`inline`} icon={faUser} size="2x" />
-                    <Link to="/profile" className={`menu-option inline`}>Profile</Link>
-                </div>
-                <div className={`menu-row inline`}>
-                    <img src={`images/more.png`} alt="more" className={`menu-more inline`}></img>
-                    <p className={`menu-option menu-more-text`} >More</p>
+                <div className={`container`}>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <FontAwesomeIcon className={`menu-home-icon`} icon={faHome} size="2x" />
+                        </div>
+                        <div className={`col menu-home-option`}>
+                            <Link to="/feed" className={`menu-home`}>Home</Link>
+                        </div>
+                    </div>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <FontAwesomeIcon className={`menu-icon`} icon={faHashtag} size="2x" />
+                        </div>
+                        <div className={`col menu-option`}>
+                            <Link to="/explore" className={`menu-option`}>Explore</Link>
+                        </div>
+                    </div>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <FontAwesomeIcon className={`menu-icon`} icon={faEnvelope} size="2x" />
+                        </div>
+                        <div className={`col menu-option`}>
+                            <Link to="/messages" className={`menu-option`}>Messages</Link>
+                        </div>
+                    </div>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <FontAwesomeIcon className={`menu-icon`} icon={faUsers} size="2x" />
+                        </div>
+                        <div className={`col menu-option`}>
+                            <Link to="/connect" className={`menu-option`}>Connect</Link>
+                        </div>
+                    </div>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <FontAwesomeIcon className={`menu-icon`} icon={faUser} size="2x" />
+                        </div>
+                        <div className={`col menu-option`}>
+                            <Link to="/profile" className={`inline`}>Profile</Link>
+                        </div>
+                    </div>
+                    <div className={`row`}>
+                        <div className={`col-3`}>
+                            <img src={`images/more.png`} alt="more" className={`menu-more menu-icon`}></img>
+                        </div>
+                        <div className={`col menu-option`}>
+                            <p className={`menu-more-text`} >More</p>  
+                        </div>
+                    </div>
                 </div>
                 
                 <div className=".btn">
