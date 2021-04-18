97e7a0a1db5dc4c73dcb29a6499c7338fd69038f
Avril <avril.van.domselaar@hotmail.com>
2021-04-17 22:32:39 -0300

    Menu styling

--- twitter_clone/src/components/Menu.js
+++ twitter_clone/src/components/Menu.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Menu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {faHashtag} from '@fortawesome/free-solid-svg-icons';
+import {faUsers} from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {faUser} from '@fortawesome/free-regular-svg-icons';
 
 class Menu extends Component{
     constructor() {
@@ -18,17 +25,35 @@ class Menu extends Component{
     // }
     render(){
         return(
-            <div className={`menu`}>
-                <ul>
-                    <li><Link to="/feed">Home</Link></li>
-                    <li><Link to="/explore">Explore</Link></li>
-                    <li><Link to="/messages">Messages</Link></li>
-                    <li><Link to="/connect">Connect</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li>More</li>
-                </ul>
+            <div className={`menu-menu`}>
+                <FontAwesomeIcon className={`menu-twitter-icon`} icon={faTwitter} size="2x" />
+                <div className={`menu-row inline`}>
+                    <FontAwesomeIcon className={`inline menu-home-icon`} icon={faHome} size="2x" />
+                    <Link to="/feed" className={`inline menu-option menu-home`}>Home</Link>
+                </div>
+                <div className={`menu-row inline`}>
+                    <FontAwesomeIcon className={`inline`} icon={faHashtag} size="2x" />
+                    <Link to="/explore" className={`menu-option`}>Explore</Link>
+                </div>
+                <div className={`menu-row inline`}>
+                    <FontAwesomeIcon className={`inline`} icon={faEnvelope} size="2x" />
+                    <Link to="/messages" className={`menu-option`}>Messages</Link>
+                </div>
+                <div className={`menu-row inline`}>
+                    <FontAwesomeIcon className={`inline`} icon={faUsers} size="2x" />
+                    <Link to="/connect" className={`menu-option`}>Connect</Link>
+                </div>
+                <div className={`menu-row inline`}>
+                    <FontAwesomeIcon className={`inline`} icon={faUser} size="2x" />
+                    <Link to="/profile" className={`menu-option inline`}>Profile</Link>
+                </div>
+                <div className={`menu-row inline`}>
+                    <img src={`images/more.png`} alt="more" className={`menu-more inline`}></img>
+                    <p className={`menu-option menu-more-text`} >More</p>
+                </div>
+                
                 <div className=".btn">
-                    <button type="button">Tweet</button>
+                    <button type="button" className={`rounded-pill menu-tweet-button`}>Tweet</button>
                 </div>
             </div>
 
